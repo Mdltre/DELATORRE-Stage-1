b5b76d6dc7fa47a5184a9f8e81ede965d16f0fe2
MJ Dela Torre <mjpdelatorre@gmail.com>
2023-06-29 13:30:50 +0800

    First stage skrr

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const PORT = 3000;
+const FAVORITE_LANGUAGE = 'Python';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`My favorite programming language is ${FAVORITE_LANGUAGE}!\n`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
